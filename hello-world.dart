a604ba5b2a4c7e8cb692dd28833836de18b653d0
Thomas Rogers <Mr.Thomas.Rogers@Proton.me>
2024-12-23 22:51:59 +0000

    playing around with some basic error handling

--- hello-world.dart
+++ hello-world.dart
@@ -3,13 +3,44 @@ import 'movie-model.dart';
 void main(){
 	print('Hello, World!');
 
-	const Map<String, dynamic> json = {
-		"title": "Movie Name"
-	};
+	List<Map<String, dynamic>> moviesData = [
+		{
+			'title': 'Inception',
+			'releaseYear': 2010,
+			'rated': 'PG-13',
+			'actors': 'Leonardo DiCaprio, Joseph Gordon-Levitt, Ellen Page',
+			'plot': 'A thief who enters the minds of others through their dreams is given the task of planting an idea into the mind of a CEO.',
+			'director': 'Christopher Nolan',
+			'stars': 9,
+			'posterUrl': 'https://example.com/inception-poster.jpg',
+		},
+		{
+			'title': 'Inception 2',
+			'releaseYear': 2015,
+			'rated': 'PG-13',
+			'actors': 'Leonardo DiCaprio, Joseph Gordon-Levitt, Ellen Page',
+			'plot': 'A thief who enters the minds of others through their dreams is given the task of planting an idea into the mind of a CEO.',
+			'director': 'Christopher Nolan',
+			'stars': 9,
+			'posterUrl': 'https://example.com/inception-poster.jpg',
+		}
+	];
 
-	Movie movie = Movie.fromJson(json);
+	List<Movie> movies = [];
 
-	print(movie);
-	print(movie.title);
+	for(final movie in moviesData){
+		try{
+			final Movie theMovie = Movie.fromJson(movie);
+			movies.add(theMovie);
+		}catch(error){
+			print("Error, movie was not added to list: $error");
+			continue;
+		}
+	}
+
+	print(movies[0]);
+	print(movies[0].title);
+
+	print("total movies: ${movies.length}");
 }
 
--- movie-model.dart
+++ movie-model.dart
@@ -1,27 +1,38 @@
 class Movie {
     final String title;
-    // final int releaseYear;
-    // final String rated;
-    // final String actors;
-    // final String? plot;
-    // final String director;
-    // final int stars;
-    // final String? posterUrl;
+    final int releaseYear;
+    final String rated;
+    final String actors;
+    final String? plot;
+    final String director;
+    final int stars;
+    final String? posterUrl;
 
     Movie._(
         this.title,
-        // this.releaseYear,
-        // this.rated,
-        // this.actors,
-        // this.plot,
-        // this.director,
-        // this.starts,
-        // this.posterUrl
+        this.releaseYear,
+        this.rated,
+        this.actors,
+        this.plot,
+        this.director,
+        this.stars,
+        this.posterUrl
     );
 
     factory Movie.fromJson(Map<String, dynamic> json){
+        json.forEach((key, value) {
+            if(value == null) throw "${key} is null in the data.";
+        });
+        
         return Movie._(
-            json["title"]
+            json["title"],
+            json["releaseYear"],
+            json["rated"],
+            json["actors"],
+            json["plot"],
+            json["director"],
+            json["stars"],
+            json["posterUrl"],
         );
     }
 }
